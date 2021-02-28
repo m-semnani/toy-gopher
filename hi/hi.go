9544458e1f3deeaf77063713eb48f7f6b9da0941
mohammad.semnani <46643886+m-semnani@users.noreply.github.com>
2021-02-28 19:58:40 +0100

    rand message added.

--- hi/hi.go
+++ hi/hi.go
@@ -2,13 +2,30 @@ package hi
 
 import (
 	"errors"
+	"fmt"
+	"math/rand"
+	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 // HelloWorld :)
 func HelloWorld(name string) (string, error) {
 	if name == "" {
-		return "", errors.New("Name can not be empty!")
+		return "", errors.New("name can not be empty")
 	}
 
-	return "Hello " + name, nil
+	return fmt.Sprintf(getRandomMessage(), name), nil
+}
+
+func getRandomMessage() string {
+	randMsg := []string{
+		"Hi %v, welcome :)",
+		"Hello there, nice to meet you %v",
+		"%v! Good morning.",
+	}
+
+	return randMsg[rand.Intn(len(randMsg))]
 }
